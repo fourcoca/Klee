5e7ff0c6590a1632d9527d6dc9e55436a078b96b
Gabriel MAYOUD <gabriel.mayoud@gmail.com>
2020-03-06 10:11:50 +0100

    HBDLB improved

--- ProjetGitGab/src/Test.java
+++ ProjetGitGab/src/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("- Salut, ça krunke ?");
-		System.out.println("- Comme un lundi !");
+		System.out.println("- Ma foi, comme un lundi ! Frais dans la matinée, soleil dans l'après-midi, la recette des sports d'hiver réussie !");
 	}
 }
