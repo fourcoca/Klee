368c0c18114912d7fe58dab52516521214056bec
Gabriel MAYOUD <gabriel.mayoud@gmail.com>
2020-03-06 10:15:48 +0100

    OSS dégradé

--- ProjetGitGab/src/Test.java
+++ ProjetGitGab/src/Test.java
@@ -5,5 +5,6 @@ public class Test {
 
 		System.out.println("- Salut, ça krunke ?");
 		System.out.println("- Ma foi, comme un lundi ! Frais dans la matinée, soleil dans l'après-midi, la recette des sports d'hiver réussie !");
+		System.out.println("- Wagon Lee hihihi");
 	}
 }
